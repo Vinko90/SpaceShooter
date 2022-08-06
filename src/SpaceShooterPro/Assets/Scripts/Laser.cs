c5e6d8428c54a1d8eb38bc5e6356e662db7c0b07
Vinko90 <vincenzobove90@gmail.com>
2022-08-06 21:37:22 +0200

    Add shooting laser functionality

---
+++ src/SpaceShooterPro/Assets/Scripts/Laser.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _laserSpeed = 8.0f;
+    
+    [SerializeField]
+    private float _autoDestroyOffset = 8.0f;
+    
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    public void Update()
+    {
+        CalculateMovement();
+        CheckDestroyOffset();
+    }
+
+    /// <summary>
+    /// Calculate laser movement and speed.
+    /// </summary>
+    private void CalculateMovement()
+    {
+        transform.Translate(Vector3.up * _laserSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Check if this laser object need to be destroyed.
+    /// </summary>
+    private void CheckDestroyOffset()
+    {
+        if (transform.position.y > _autoDestroyOffset)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- src/SpaceShooterPro/Assets/Scripts/Player.cs
+++ src/SpaceShooterPro/Assets/Scripts/Player.cs
@@ -2,21 +2,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float PlayerSpeed = 3.5f;
+    #region Editor Settings
+    [SerializeField]
+    private float _playerSpeed = 3.5f;
 
-    public float VerticalMaxBoundary = 0;
+    [SerializeField]
+    private float _verticalMaxBoundary = 0;
 
-    public float VerticalMinBoundary = -3.8f;
+    [SerializeField]
+    private float _verticalMinBoundary = -3.8f;
 
-    public float HorizontalMaxBoundary = 11.3f;
+    [SerializeField]
+    private float _horizontalMaxBoundary = 11.3f;
 
-    public float HorizontalMinBoundary = -11.3f;
+    [SerializeField]
+    private float _horizontalMinBoundary = -11.3f;
+
+    [SerializeField]
+    private float _laserSpawnOffset = 0.8f;
+    
+    [SerializeField] 
+    private float _fireRate = 0.15f;
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+    #endregion
+    
+    private float _canFire = -1f;
     
     /// <summary>
     /// Start is called before the first frame update.
     /// Position player to center screen.
     /// </summary>
-    void Start()
+    private void Start()
     {
         transform.position = new Vector3(0, 0, 0);
     }
@@ -24,39 +42,54 @@ public class Player : MonoBehaviour
     /// <summary>
     /// Update is called once per frame.
     /// </summary>
-    void Update()
+    private void Update()
     {
         CalculateMovement();
+        
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
     }
 
     /// <summary>
     /// Get vertical and horizontal movement value from key manager
     /// and position player within boundaries.
     /// </summary>
-    void CalculateMovement()
+    private void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 newPlayerPosition = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate(newPlayerPosition * PlayerSpeed * Time.deltaTime);
+        transform.Translate(newPlayerPosition * _playerSpeed * Time.deltaTime);
         
         //Clamp Y Boundaries
         transform.position = new Vector3
            {
                x = transform.position.x,
-               y = Mathf.Clamp(transform.position.y, VerticalMinBoundary, VerticalMaxBoundary),
+               y = Mathf.Clamp(transform.position.y, _verticalMinBoundary, _verticalMaxBoundary),
                z = 0
            };
         
         //Check X Boundaries
-        if (transform.position.x > HorizontalMaxBoundary)
+        if (transform.position.x > _horizontalMaxBoundary)
         {
-            transform.position = new Vector3(HorizontalMinBoundary, transform.position.y, 0);
+            transform.position = new Vector3(_horizontalMinBoundary, transform.position.y, 0);
         }
-        else if (transform.position.x < HorizontalMinBoundary)
+        else if (transform.position.x < _horizontalMinBoundary)
         {
-            transform.position = new Vector3(HorizontalMaxBoundary, transform.position.y, 0);
+            transform.position = new Vector3(_horizontalMaxBoundary, transform.position.y, 0);
         }
+    }
+
+    /// <summary>
+    /// Instantiate laser capsule when pressing space-bar
+    /// </summary>
+    private void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Vector3 spawnLocation = transform.position + new Vector3(0, _laserSpawnOffset, 0);
+        Instantiate(_laserPrefab, spawnLocation, Quaternion.identity);
     }
 }
